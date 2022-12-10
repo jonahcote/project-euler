0cc00084d2fc1604783e87a1870f3ab031290384
jonahcote <jonahwcote@gmail.com>
2022-12-10 13:34:39 -0500

    rewrittern with sqrt max & nested checks, done

--- p3.py
+++ p3.py
@@ -2,97 +2,39 @@
 
 # What is the largest prime factor of the number 600851475143 ?
 
-# Strategy:
-# Build a list of all prime numbers below half of n
-#   - Sieve of Eratosthenes - eliminate multiples of 2, 3, 4...
-#   - start out with array of 1s of size n/2
-#   - for each index that's a multiple of m, change the value to 0
-#   - repeat, increasing index by 1 up to n/2
-#   - at the end, all remaining 1 values are prime
-# Build a list of all factors of n
-#   - start out with array of 0s of size n/2
-#   - for each factor f, if f % n == 0, set value to a 1
-# Find matches of the prime number array and factor array
-#   - for each item equal to 1 in array primes, check corresponding index in array factors
-#   - if match, add to array matches
-# Take the last value of that arrray and return it
-
-# Strategy pt. 2
-# Find a list of all the factors
-# Plug that list into a function that checks if a number is prime or not
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-
 # imports
 import numpy as np
+import math
 
 # define main
 def main():
+
     n = 600851475143
-    # generatePrimes(n)
-    # primes = np.array(generatePrimes(n), dtype=np.int64)
-    factors = np.array(generateFactors(n), dtype=np.int64)
-    print(factors)
-    # overlap = np.array([], dtype=np.int64)
-    # overlap = np.intersect1d(primes, factors)
-    # print("Overlap:\n",overlap)
-    # final = overlap[-1]
-    # print(final)
+    gpf = 1
 
-
-
-# Build a list of all prime numbers below half of n
-def generatePrimes(n):
-
-    # generate list of potential primes (all numbers 2-n\\2)
-    floor = 2
-    ceiling = n//2 + 1
-    a = np.arange(floor, ceiling, 1)
-
-    # for 
-
-    print(a)
-
-
-    # # Sieve of Eratosthenes - eliminate multiples of 2, 3, 4...
-    # # start out with array of 1s of size n/2
-    # a = np.ones((n//2), dtype=np.int16)
-    # # for each index that's a multiple of m, change the value to 0
-    # m = 2
-    # while (m < n//2):
-    #     i = 2
-    #     while (i*m < n//2):
-    #         a[i*m] = 0
-    #         i += 1
-    #     m += 1
-    
-    # # return array of 1 value indexes (array of just primes)
-    # for i in range(len(a)):
-    #     if a[i] == 1:
-    #         a[i] = i
-    # a = a[a != 0]
-    # return(a)
-
-def generateFactors(n):
-    # create an array of zeros
-    a = np.array([], dtype=np.int16)
-    # we have 500 zeros
-    # we need to find numbers that are factors of n
-    # for every number 2-n//2, run n % that number == 0
+    # check every i between 2 and sqrt(n)
     i = 2
-    while(i <= n//2):
-        
+    ceiling1 = math.sqrt(n)
+    print("n\t",n,"\ngpf\t",gpf,"\ni\t",i,"\nc1\t",ceiling1,"\n")
+    while(i <= ceiling1):
+        # check if i is a factor of n
         if(n % i == 0):
-            a = np.append(a, i)
+            print("Factor found:",i)
+            # if so, check if i has any factors between 2 and sqrt(i)
+            j = 2
+            ceiling2 = math.sqrt(i)
+            while(j <= ceiling2):
+                if(i % j == 0):
+                    # if so, it's not prime
+                    break
+                j += 1
+            # if not at the end, replace gpf with i
+            else:
+                print("\t\t\t",i,"is prime.")
+                gpf = i
         i += 1
-    return a
+
+    print("\n=======\nFINAL:",gpf,"\n")
 
 # call main
 main()
