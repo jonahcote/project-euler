dc12f3ebae93e4ceda887f492c8ce825881c0177
jonahcote <jonahwcote@gmail.com>
2022-12-10 12:30:44 -0500

    functional for small sizes

--- py3.py
+++ py3.py
@@ -17,34 +17,56 @@
 #   - if match, add to array matches
 # Take the last value of that arrray and return it
 
+# imports
+import numpy as np
+
 # define main
 def main():
-    generatePrimes(1000)
+    n = 13195
+    primes = np.array(generatePrimes(n), dtype=np.int64)
+    factors = np.array(generateFactors(n), dtype=np.int64)
+    overlap = np.array([], dtype=np.int64)
+    overlap = np.intersect1d(primes, factors)
+    print("Overlap:\n",overlap)
+    final = overlap[-1]
+    print(final)
+
+
 
 # Build a list of all prime numbers below half of n
 def generatePrimes(n):
     # Sieve of Eratosthenes - eliminate multiples of 2, 3, 4...
     # start out with array of 1s of size n/2
-    startArray = []
-    for i in range(n//2):
-        startArray.append(1)
+    a = np.ones((n//2), dtype=np.int16)
     # for each index that's a multiple of m, change the value to 0
     m = 2
     while (m < n//2):
         i = 2
         while (i*m < n//2):
-            startArray[i*m] = 0
-            # print("Removed value at:",i*m)
+            a[i*m] = 0
             i += 1
         m += 1
-    print(startArray)
     
     # return array of 1 value indexes (array of just primes)
-    for i in range(len(startArray)):
-        if startArray[i] == 1:
-            startArray[i] = i
-            
-    print(startArray)
+    for i in range(len(a)):
+        if a[i] == 1:
+            a[i] = i
+    a = a[a != 0]
+    return(a)
+
+def generateFactors(n):
+    # create an array of zeros
+    a = np.array([], dtype=np.int16)
+    # we have 500 zeros
+    # we need to find numbers that are factors of n
+    # for every number 2-n//2, run n % that number == 0
+    i = 2
+    while(i <= n//2):
+        
+        if(n % i == 0):
+            a = np.append(a, i)
+        i += 1
+    return a
 
 # call main
 main()
