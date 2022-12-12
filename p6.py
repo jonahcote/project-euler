f0f26de7a80486f56382ba09d7bd011a51e76b1e
jonahcote <jonahwcote@gmail.com>
2022-12-12 13:37:02 -0500

    Finished

--- p6.py
+++ p6.py
@@ -5,21 +5,29 @@
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
+start_value = 1
+end_value = 100
+
 sum_of_squares = 0
-
-for i in range(1,100):
-    print("Adding",i**2)
-    sum_of_squares += i ** 2
-print("Sum of squares:",sum_of_squares)
-
 sum_of_natural_numbers = 0
+square_of_sum = 0
 
-for i in range(1,100):
-    print("Adding",i**2)
+# Find the sum of the squares
+# Iterate through each natural number, square it, and add it to sum_of_squares 
+for i in range(start_value, end_value+1):
+    #print("Adding",i**2)
+    sum_of_squares += i ** 2
+#print("Sum of squares:",sum_of_squares)
+
+
+# Find the square of the sum
+# Itereate through each natural number, and add it to sum_of_natural_numbers
+for i in range(start_value, end_value+1):
+    #print("Adding",i)
     sum_of_natural_numbers += i
-print("Sum of natural numbers:",sum_of_natural_numbers)
+#print("Sum of natural numbers:",sum_of_natural_numbers)
 
 square_of_sum = sum_of_natural_numbers ** 2
-print("Square of sum:",square_of_sum)
+#print("Square of sum:",square_of_sum)
 
-print(abs(square_of_sum-sum_of_squares))
+print("Difference:",abs(square_of_sum-sum_of_squares))
